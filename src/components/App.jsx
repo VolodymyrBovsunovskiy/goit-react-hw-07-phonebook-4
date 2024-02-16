48100d5f8bd08ab0fcba46f5928adc35cbba141f
VolodymyrBovsunovskiy <102419980+VolodymyrBovsunovskiy@users.noreply.github.com>
2024-02-16 12:35:40 +0200

    qwe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import { Wrapper } from './Wrapper/Wrapper';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import { CircleLoader } from 'react-spinners';
+import { selectIsLoading } from './redux/contacts.selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from './redux/contacts';
+
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading === true && (
+        <CircleLoader
+          color="#670063"
+          size={100}
+          cssOverride={{
+            margin: '30px auto',
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}
+        />
+      )}
+      <ContactsList />
+    </Wrapper>
   );
 };
